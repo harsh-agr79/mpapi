a7f70325484d8176e41dcf2b8cbcd0edce8981ae
harsh-agr79 <agrharsh7932@gmail.com>
2024-12-30 12:27:41 +0530

    changes

---
+++ app/Policies/PoliciesPolicy.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Policy;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PoliciesPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        if($user->hasPermissionTo('View Policy')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Policy $policy): bool
+    {
+        if($user->hasPermissionTo('View Policy')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        if($user->hasPermissionTo('Create Policy')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Policy $policy): bool
+    {
+        if($user->hasPermissionTo('Edit Policy')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Policy $policy): bool
+    {
+        if($user->hasPermissionTo('Delete Policy')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Policy $policy): bool
+    {
+        if($user->hasPermissionTo('Delete Policy')){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Policy $policy): bool
+    {
+        if($user->hasPermissionTo('Delete Policy')){
+            return true;
+        }
+        return false;
+    }
+}
