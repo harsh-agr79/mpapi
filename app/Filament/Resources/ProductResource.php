02eaf2985ee23b8535cfcce03a72854374e30929
harsh-agr79 <agrharsh7932@gmail.com>
2025-01-29 00:13:06 +0530

    changes

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -34,7 +34,7 @@ class ProductResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?int $navigationSort = 6;
-    protected static ?string $navigationGroup = 'Inventory';
+    protected static ?string $navigationGroup = 'Inventor y';
 
     public static function form(Form $form): Form
     {
