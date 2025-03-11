4839877bfadd0f016b5f9ff360c2dac5abfe4a08
harsh-agr79 <agrharsh7932@gmail.com>
2025-03-11 23:13:54 +0530

    changes

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -20,7 +20,9 @@ class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
+
+    protected static ?int $navigationSort = 1;
 
     public static function form(Form $form): Form
     {
