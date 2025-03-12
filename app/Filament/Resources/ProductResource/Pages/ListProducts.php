1a9134d25a29548981583fa42d878206440de535
harsh-agr79 <agrharsh7932@gmail.com>
2025-03-12 14:39:12 +0530

    changes

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -4,10 +4,14 @@ namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use App\Models\Category;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
+use Filament\Resources\Components\Tab;
 
 class ListProducts extends ListRecords
 {
+    use ExposesTableToWidgets;
     protected static string $resource = ProductResource::class;
 
     protected function getHeaderActions(): array
@@ -15,5 +19,17 @@ class ListProducts extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = [];
+        $categories = Category::all();
+        foreach ($categories as $category) {
+            $tabs["category_{$category->id}"] = Tab::make($category->name)
+                ->query(fn ($query) => $query->where('category_id', $category->id));
+        }
+
+        return $tabs;
     }
 }
