23fa09c134f28af9fe4a5d65f600b367ad80a73a
harsh-agr79 <agrharsh7932@gmail.com>
2025-03-13 23:26:43 +0530

    changes

---
+++ database/migrations/2025_03_13_175540_update_foreign_key_in_order_items.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->dropForeign(['customer_id']);
+            
+            // Add the new foreign key
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->dropForeign(['customer_id']);
+            $table->foreign('customer_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+};
