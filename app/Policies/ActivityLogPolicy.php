8e7596b4650580130855b73b3ca72673d7ff7a6d
iNeatz <sahilshrestha77@gmail.com>
2025-05-06 22:23:35 +0545

    policies renamed

--- app/Policies/ActivityLogPolicy.php
+++ app/Policies/ActivityLogPolicy.php
@@ -6,7 +6,7 @@ use App\Models\ActivityLog;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class ActivityLogsPolicy
+class ActivityLogPolicy
 {
     /**
      * Determine whether the user can view any models.
--- app/Policies/PolicyPolicy.php
+++ app/Policies/PolicyPolicy.php
@@ -6,7 +6,7 @@ use App\Models\Policy;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class PoliciesPolicy
+class PolicyPolicy
 {
     /**
      * Determine whether the user can view any models.
