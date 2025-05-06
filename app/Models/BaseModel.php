38620747dc126310199ae3f02d1adfb96c92f529
iNeatz <sahilshrestha77@gmail.com>
2025-05-06 21:55:57 +0545

    Customer logs error fallback

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -32,6 +32,11 @@ class BaseModel extends Model
          return;
       }
 
+      $user = Auth::user();
+      if (!$user instanceof \App\Models\User) {
+         return;
+      }
+
       ActivityLog::create([
          'table_name' => $model->getTable(),
          'primary_key_value' => $model->getKey(),
