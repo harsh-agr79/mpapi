cc892f6efe0a394ce6a1de27e6e3315f5723d794
iNeatz <sahilshrestha77@gmail.com>
2025-05-10 16:20:11 +0545

    Infolist for orders table

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -6,6 +6,11 @@ use App\Filament\Resources\ActivityLogRelationManagerResource\RelationManagers\A
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
 use Filament\Forms\Components\{TextInput, Select, DatePicker, Section, Toggle, Textarea, Grid};
+use Filament\Infolists\Components\KeyValueEntry;
+use Filament\Infolists\Components\RepeatableEntry;
+use Filament\Infolists\Components\Section as InfoSection;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\{TextColumn, BadgeColumn};
 use Filament\Tables\Filters\Filter;
 use App\Models\Order;
@@ -92,7 +97,7 @@ class OrderResource extends Resource
 
                     TextInput::make('coupon_code')
                         ->label('Coupon Applied')
-                        
+
                         ->readOnly(),
 
                     TextInput::make('coupon_discount')
@@ -204,6 +209,121 @@ class OrderResource extends Resource
             ], layout: FiltersLayout::AboveContentCollapsible)
             ->filtersFormColumns(3)
             ->actions([
+                ViewAction::make()
+                    ->modalHeading(fn($record) => 'Order: #' . ucfirst($record->id))
+                    ->modalSubmitAction(false)
+                    ->modalCancelActionLabel('Close')
+                    ->infolist([
+                        InfoSection::make()
+                            ->schema([
+
+                                TextEntry::make('id')->label('ORDER ID')
+                                    ->getStateUsing(fn($record) => '#' . $record->id),
+                                TextEntry::make('order_date')->label('ORDER DATE'),
+                                TextEntry::make('billing_full_name')->label('BILLED TO'),
+                                TextEntry::make('billing_address')->label('BILLING ADDRESS')
+                                    ->getStateUsing(function ($record) {
+                                        $parts = array_filter([
+                                            $record->billing_street_address,
+                                            $record->billing_municipality,
+                                            $record->billing_city,
+                                            $record->billing_state,
+                                            $record->billing_country_region,
+                                            $record->billing_postal_code,
+                                        ]);
+
+                                        return implode(', ', $parts);
+                                    })
+                                    ->hidden(fn($record) => empty(array_filter([
+                                        $record->billing_street_address,
+                                        $record->billing_municipality,
+                                        $record->billing_city,
+                                        $record->billing_state,
+                                        $record->billing_country_region,
+                                        $record->billing_postal_code,
+                                    ])))
+                                    ->extraAttributes(['class' => 'capitalize']),
+                                TextEntry::make('shipping_full_name')->label('SHIP TO'),
+                                TextEntry::make('shipping_address')->label('SHIPPING ADDRESS')
+                                    ->getStateUsing(function ($record) {
+                                        $parts = array_filter([
+                                            $record->shipping_street_address,
+                                            $record->shipping_municipality,
+                                            $record->shipping_city,
+                                            $record->shipping_state,
+                                            $record->shipping_country_region,
+                                            $record->shipping_postal_code,
+                                        ]);
+
+                                        return implode(', ', $parts);
+                                    })
+                                    ->hidden(fn($record) => empty(array_filter([
+                                        $record->shipping_street_address,
+                                        $record->shipping_municipality,
+                                        $record->shipping_city,
+                                        $record->shipping_state,
+                                        $record->shipping_country_region,
+                                        $record->shipping_postal_code,
+                                    ])))
+                                    ->extraAttributes(['class' => 'capitalize']),
+                                TextEntry::make('current_status')->label('ORDER STATUS')
+                                    ->badge()
+                                    ->color(fn(string $state): string => match ($state) {
+                                        'pending' => 'warning',
+                                        'approved' => 'info',
+                                        'processing' => 'info',
+                                        'packing' => 'secondary',
+                                        'shipped' => 'primary',
+                                        'delivered' => 'success',
+                                        'cancelled' => 'danger',
+                                        'returned' => 'danger',
+                                        'refunded' => 'dark',
+                                    }),
+                                TextEntry::make('payment_status')->label('PAYMENT STATUS')
+                                    ->badge()
+                                    ->color(fn(string $state): string => match ($state) {
+                                        'pending' => 'warning',
+                                        'paid' => 'success',
+                                        'cod' => 'info',
+                                    }),
+                                RepeatableEntry::make('OrderItem')
+                                    ->label('Order Items')
+                                    ->columnSpanFull()
+                                    ->schema([
+                                        TextEntry::make('product.name')->label('Item Name'),
+                                        TextEntry::make('quantity')->label('Quantity'),
+                                        TextEntry::make('color')->label('Color')
+                                            ->visible(fn($record) => filled($record->color)),
+                                        TextEntry::make('price')->label('Price')->money('npr'),
+                                        TextEntry::make('discounted_price')->label('Discounted Price')->money('npr')
+                                            ->money('npr'),
+                                        TextEntry::make('line_total')->label('Line Total')->money('npr')
+                                            ->getStateUsing(fn($record) => $record->quantity * $record->discounted_price)
+                                        ,
+                                    ])
+                                    ->columns(3),
+                                KeyValueEntry::make('amounts')
+                                    ->label('AMOUNTS')
+                                    ->state(function ($record) {
+                                        return [
+                                            'SUB-TOTAL' => 'NPR ' . number_format($record->total_amount, 2),
+                                            'DISCOUNT' => 'NPR ' . number_format($record->discount, 2),
+                                            'TOTAL' => 'NPR ' . number_format($record->discounted_total, 2),
+                                            'COUPON CODE' => $record->coupon_code,
+                                            'COUPON DISCOUNT' => 'NPR ' . $record->coupon_discount,
+                                            'DELIVERY FEE' => 'NPR ' . number_format($record->delivery_charge, 2),
+                                            'NET TOTAL' => 'NPR ' . number_format($record->net_total, 2),
+                                        ];
+                                    })
+                                    ->columnSpanFull()
+                                ,
+                                TextEntry::make('created_at')->label('CREATED_AT'),
+                                TextEntry::make('updated_at')->label('UPDATED_AT'),
+                                // TextEntry::make('deleted_at')->label('DELETED_AT')->visible(fn($record) => filled($record->deleted_at)),
+
+                            ])
+                            ->columns(2),
+                    ]),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
